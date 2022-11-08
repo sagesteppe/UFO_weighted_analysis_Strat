3103d48b530010aba32ca98a07991941ce3f0f7b
sagesteppe <reedbenkendorf27@gmail.com>
2022-11-08 14:31:32 -0700

    issues with turning the process into a function, need to ensure that ALL datasets are in matched order

---
+++ scripts/functions.R
@@ -0,0 +1,58 @@
+plotWeigher <- function(x, pts){
+  
+  # this function moves an Original AIM sample design through the process of re-weighing
+  # the acre points based on which base points were sampled or not. It utilizes the
+  # 'adjwgt' function from spsurvey just for official book keeping. It mostly works as 
+  # a wrapper to reduce the number of lines in a document dedicated to dealing with
+  # multiple weighings. 
+  
+  pts_base <- filter(pts, str_detect(Panel, 'OverSample', negate = T))
+  
+  aim_sites <- pts_base %>%
+    mutate(Plot.Status = if_else(Plot.Status == 'sampled', T, F)) %>% 
+    pull(Plot.Status, stratum)
+  aim_wgtcat <- pull(pts_base, stratum)
+  
+  aim_wgt <- x %>% # acreage and proportion give the same results. 
+    select(PropTarget, Stratum) %>% 
+    left_join(., pts_base %>% select(stratum), by = c('Stratum' = 'stratum')) %>% 
+    mutate(Stratum = factor(Stratum, levels = (unique(aim_wgtcat))))  %>% 
+    arrange(Stratum) %>% 
+    pull(PropTarget, Stratum) 
+  
+  aim_framesize <- pull(x, DesiredSS, Stratum)
+  aim_framesize <- aim_framesize[match(unique(aim_wgtcat),names(aim_framesize))]
+  
+  res <- 1/adjwgt(aim_wgt, aim_wgtcat, framesize = aim_framesize, sites = aim_sites) 
+  names(res) <- aim_wgtcat
+  
+  newWghts <- data.frame(
+    'Stratum' = names(res),
+    'Weight' = res) %>% 
+    group_by(Stratum) %>% 
+    filter(Weight < Inf) %>% 
+    add_count(name = 'PlotsSampled') %>% 
+    distinct(.keep_all = T) %>% 
+    mutate(WghtPerPlot = Weight / PlotsSampled)
+  
+  newWghts1 <- data.frame(
+    'Stratum' = names(res),
+    'Weight' = res) %>% 
+    group_by(Stratum, Weight) %>%
+    filter(Weight == Inf) %>% 
+    add_count(name = 'PlotsRejected')  %>% 
+    distinct(.keep_all = T) %>% 
+    ungroup %>% 
+    select(-Weight)
+  
+  newWghts <- left_join(newWghts, newWghts1) %>% 
+  left_join(., OriginalWeights %>% 
+              select(TotalAcres = Acres, Rejected) ) %>% 
+    mutate(WgtAcres = WghtPerPlot * TotalAcres, 
+           AreaInference = TotalAcres * Weight)  %>% 
+    select(Stratum, TotalAcres, AreaInference, WgtAcres, 
+           PlotsSampled, PlotsRejected, PropInference = Weight, WghtPerPlot) 
+  
+  return(newWghts)
+  
+}

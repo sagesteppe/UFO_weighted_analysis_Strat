4568e0b3d0b999005b0e78afa507d48a7cd585b0
sagesteppe <reedbenkendorf27@gmail.com>
2022-11-08 15:27:18 -0700

    dealt with ordering all vectors, some cornercase maths correction thrown in

--- scripts/functions.R
+++ scripts/functions.R
@@ -6,25 +6,37 @@ plotWeigher <- function(x, pts){
   # a wrapper to reduce the number of lines in a document dedicated to dealing with
   # multiple weighings. 
   
-  pts_base <- filter(pts, str_detect(Panel, 'OverSample', negate = T))
+  pts_base <- filter(wsAcec_pts, str_detect(Panel, 'OverSample', negate = T))
   
   aim_sites <- pts_base %>%
     mutate(Plot.Status = if_else(Plot.Status == 'sampled', T, F)) %>% 
     pull(Plot.Status, stratum)
   aim_wgtcat <- pull(pts_base, stratum)
   
-  aim_wgt <- x %>% # acreage and proportion give the same results. 
+  aim_wgt <- wsAcec_OriginalWeights %>% # acreage and proportion give the same results. 
     select(PropTarget, Stratum) %>% 
     left_join(., pts_base %>% select(stratum), by = c('Stratum' = 'stratum')) %>% 
     mutate(Stratum = factor(Stratum, levels = (unique(aim_wgtcat))))  %>% 
     arrange(Stratum) %>% 
-    pull(PropTarget, Stratum) 
+    pull(PropTarget, Stratum)
   
-  aim_framesize <- pull(x, DesiredSS, Stratum)
+  aim_framesize <- pull(wsAcec_OriginalWeights, DesiredSS, Stratum)
+  
+  aim_sites <- aim_sites[order(names(aim_sites))] 
+  aim_wgt <- aim_wgt[order(names(aim_wgt))] 
+  aim_wgtcat <- aim_wgtcat[order(aim_wgtcat)]
   aim_framesize <- aim_framesize[match(unique(aim_wgtcat),names(aim_framesize))]
   
-  res <- 1/adjwgt(aim_wgt, aim_wgtcat, framesize = aim_framesize, sites = aim_sites) 
+  aim_wgt <- aim_wgt[!is.na(names(aim_wgt))]
+  
+  res <- adjwgt(aim_wgt, aim_wgtcat, framesize = aim_framesize, sites = aim_sites) 
   names(res) <- aim_wgtcat
+  
+  res <- ifelse(res > 1, 1/res, res)
+  
+  
+  # RES NOT SORTING CORRECTLY... LET'S RUN FULL MATCHES ON ALL THE DATA GOING INTO THE FUNCTION
+  
   
   newWghts <- data.frame(
     'Stratum' = names(res),
@@ -46,12 +58,16 @@ plotWeigher <- function(x, pts){
     select(-Weight)
   
   newWghts <- left_join(newWghts, newWghts1) %>% 
-  left_join(., OriginalWeights %>% 
-              select(TotalAcres = Acres, Rejected) ) %>% 
+    left_join(., OriginalWeights %>% 
+                select(TotalAcres = Acres, Rejected) ) %>% 
+    arrange(-Weight) %>%  # this works to catch any
+    group_by(Stratum) %>% # values which may be introduced
+    slice_head(n = 1) %>%  # by an empty second data.frame
     mutate(WgtAcres = WghtPerPlot * TotalAcres, 
            AreaInference = TotalAcres * Weight)  %>% 
     select(Stratum, TotalAcres, AreaInference, WgtAcres, 
            PlotsSampled, PlotsRejected, PropInference = Weight, WghtPerPlot) 
+  
   
   return(newWghts)
   
